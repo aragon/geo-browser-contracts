8ab53968f91267c78ae9543618a74dd3ae3840a3
Jør∂¡ <itsbrickpop@gmail.com>
2023-08-24 15:42:23 +0100

    Accepting subspaces

--- packages/contracts/src/SpacePlugin.sol
+++ packages/contracts/src/SpacePlugin.sol
@@ -6,33 +6,62 @@ import {IDAO, PluginUUPSUpgradeable} from "@aragon/osx/core/plugin/PluginUUPSUpg
 /// @title SpacePlugin
 /// @dev Release 1, Build 1
 contract SpacePlugin is PluginUUPSUpgradeable {
-    bytes32 public constant STORE_PERMISSION_ID = keccak256("STORE_PERMISSION");
+    bytes32 public constant CONTENT_PERMISSION_ID = keccak256("CONTENT_PERMISSION");
+    bytes32 public constant SUBSPACE_PERMISSION_ID = keccak256("SUBSPACE_PERMISSION");
 
-    uint256 public number; // added in build 1
+    /// @notice Emitted when the contents of a space change.
+    /// @param blockIndex The index of the block whose items have new contents.
+    /// @param itemIndex The index of the item that has new contents.
+    /// @param contentUri An IPFS URI pointing to the new contents behind the block's item.
+    event ContentChanged(uint32 blockIndex, uint32 itemIndex, string contentUri);
 
-    /// @notice Emitted when a number is stored.
-    /// @param number The number.
-    event NumberStored(uint256 number);
+    /// @notice Emitted when the DAO accepts another DAO as a subspace.
+    /// @param dao The address of the DAO to be accepted as a subspace.
+    event SubspaceAccepted(address dao);
+
+    /// @notice Emitted when the DAO stops recognizing another DAO as a subspace.
+    /// @param dao The address of the DAO to be removed as a subspace.
+    event SubspaceRemoved(address dao);
 
     constructor() {
         _disableInitializers();
     }
 
     /// @notice Initializes the plugin when build 1 is installed.
-    /// @param _number The number to be stored.
-    function initialize(IDAO _dao, uint256 _number) external initializer {
+    /// @param _dao The address of the DAO to read the permissions from.
+    /// @param _firstBlockContentUri A IPFS URI pointing to the contents of the first block's item (title).
+    function initialize(IDAO _dao, string memory _firstBlockContentUri) external initializer {
         __PluginUUPSUpgradeable_init(_dao);
-        number = _number;
 
-        emit NumberStored({number: _number});
+        emit ContentChanged({blockIndex: 0, itemIndex: 0, contentUri: _firstBlockContentUri});
     }
 
-    /// @notice Stores a new number to storage. Caller needs STORE_PERMISSION.
-    /// @param _number The number to be stored.
-    function storeNumber(uint256 _number) external auth(STORE_PERMISSION_ID) {
-        number = _number;
+    /// @notice Emits an event with new contents for the given block index. Caller needs CONTENT_PERMISSION.
+    /// @param _blockIndex The index of the block whose items have new contents.
+    /// @param _itemIndex The index of the item that has new contents.
+    /// @param _contentUri An IPFS URI pointing to the new contents behind the block's item.
+    function setContent(
+        uint32 _blockIndex,
+        uint32 _itemIndex,
+        string memory _contentUri
+    ) external auth(CONTENT_PERMISSION_ID) {
+        emit ContentChanged({
+            blockIndex: _blockIndex,
+            itemIndex: _itemIndex,
+            contentUri: _contentUri
+        });
+    }
 
-        emit NumberStored({number: _number});
+    /// @notice Emits an event accepting another DAO as a subspace. Caller needs CONTENT_PERMISSION.
+    /// @param _dao The address of the DAO to accept as a subspace.
+    function acceptSubspace(address _dao) external auth(SUBSPACE_PERMISSION_ID) {
+        emit SubspaceAccepted(_dao);
+    }
+
+    /// @notice Emits an event removing another DAO as a subspace. Caller needs CONTENT_PERMISSION.
+    /// @param _dao The address of the DAO to remove as a subspace.
+    function removeSubspace(address _dao) external auth(SUBSPACE_PERMISSION_ID) {
+        emit SubspaceRemoved(_dao);
     }
 
     /// @notice This empty reserved space is put in place to allow future versions to add new variables without shifting down storage in the inheritance chain (see [OpenZeppelin's guide about storage gaps](https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps)).
--- packages/contracts/src/SpaceVotingPlugin.sol
+++ packages/contracts/src/SpaceVotingPlugin.sol
@@ -6,14 +6,6 @@ import {IDAO, PluginUUPSUpgradeable} from "@aragon/osx/core/plugin/PluginUUPSUpg
 /// @title SpaceVotingPlugin
 /// @dev Release 1, Build 1
 contract SpaceVotingPlugin is PluginUUPSUpgradeable {
-    bytes32 public constant CONTENT_PERMISSION_ID = keccak256("CONTENT_PERMISSION");
-    bytes32 public constant SUBSPACE_PERMISSION_ID = keccak256("SUBSPACE_PERMISSION");
-
-    /// @notice Emitted when the contents of a space change.
-    /// @param blockIndex The index of the block that has new contents.
-    /// @param contentUri The IPFS URI pointing to the new contents.
-    event ContentChanged(uint32 blockIndex, string contentUri);
-
     constructor() {
         _disableInitializers();
     }
@@ -23,18 +15,6 @@ contract SpaceVotingPlugin is PluginUUPSUpgradeable {
     /// @param _firstBlockContentUri A IPFS URI pointing to the contents of the very first block.
     function initialize(IDAO _dao, string memory _firstBlockContentUri) external initializer {
         __PluginUUPSUpgradeable_init(_dao);
-
-        emit ContentChanged({blockIndex: 0, contentUri: _firstBlockContentUri});
-    }
-
-    /// @notice Emits an event with new contents for the given block index. Caller needs CONTENT_PERMISSION.
-    /// @param _blockIndex The index of the block whose contents are being changed.
-    /// @param _contentUri An IPFS URI pointing to the new contents behind the block.
-    function setContent(
-        uint32 _blockIndex,
-        string memory _contentUri
-    ) external auth(CONTENT_PERMISSION_ID) {
-        emit ContentChanged({blockIndex: _blockIndex, contentUri: _contentUri});
     }
 
     /// @notice This empty reserved space is put in place to allow future versions to add new variables without shifting down storage in the inheritance chain (see [OpenZeppelin's guide about storage gaps](https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps)).
